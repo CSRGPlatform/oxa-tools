e051723fa3566d7400f9ed96d24c6303d0f1c8a1
John Patrick Papares <johnpatrickpapares@gmail.com>
2020-06-29 10:35:24 +0800

    Some Basic Changes

--- config/stamp/default/bvt.sh
+++ config/stamp/default/bvt.sh
@@ -17,9 +17,9 @@ ENVIRONMENT=bvt
 
 # the site certificate files (.crt & .key) have to be named after the BASE_URL value
 BASE_URL=cloudswyft.com
-LMS_URL=mytest.$BASE_URL
-CMS_URL=mytestcms.$BASE_URL
-PREVIEW_URL=mytestpreview.$BASE_URL
+LMS_URL=aiacloudstreet.$BASE_URL
+CMS_URL=aiacloudstreet-studio.$BASE_URL
+PREVIEW_URL=aiacloudstreet-preview.$BASE_URL
 
 # deployment configuration
 # URL-building will be replaced by the use of LMS_URL | CMS_URL (above)
@@ -46,11 +46,11 @@ EDXAPP_EMAIL_TECH_SUPPORT="${PLATFORM_EMAIL}"
 EDXAPP_EMAIL_UNIVERSITY="${PLATFORM_EMAIL}"
 
 # config/versions.yml
-CONFIGURATION_REPO=https://github.com/mycstest/edx-configuration.git
+CONFIGURATION_REPO=https://github.com/CSRGPlatform/edx-configuration.git
 CONFIGURATION_VERSION=oxa/master.fic
-PLATFORM_REPO=https://github.com/mycstest/edx-platform.git
+PLATFORM_REPO=https://github.com/CSRGPlatform/edx-platform.git
 PLATFORM_VERSION=oxa/master.fic
-THEME_REPO=https://github.com/Microsoft/edx-theme.git
+THEME_REPO=https://github.com/CSRGPlatform/edx-theme.git
 THEME_VERSION=oxa/master.fic
 EDX_VERSION=open-release/ficus.master
 FORUM_VERSION=oxa/master.fic
